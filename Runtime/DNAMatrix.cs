a2b57d3f25606a79a6eab555629b19791196accd
Alexandre Dufresne Nappert <93613553+MrDNAlex@users.noreply.github.com>
2024-01-03 21:27:27 -0500

    Package and Namespace Name Change

--- Runtime/DNAMatrix.cs
+++ Runtime/DNAMatrix.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.IO;
 using System;
 
-namespace DNAMatrices
+namespace MachineLearningMath
 {
 
     /// <summary>
--- Runtime/DNAMatrixFloat.cs
+++ Runtime/DNAMatrixFloat.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System;
 
-namespace DNAMatrices
+namespace MachineLearningMath
 {
 
     /// <summary>
